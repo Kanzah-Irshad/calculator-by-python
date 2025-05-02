7277acb097512469524beeb0f1e862a085a31453
KANZAH IRSHAD <kanzahirshad@gmail.com>
2025-05-02 19:22:54 +0500

    app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ elif choice == '4':
     if num2 != 0: # ye not equal to check krne k leye lagaya k num2 0 na ho warna error aayega cz division by zero is not possible
         result = num1 / num2
         print(f"The result of division is: {result}")
-# agar '4' choose kiya, toh division hoga.
+# agar '4' choose kiya, toh division hoga
     else :
         print("Error: A number cannot be divided by 0")
     # agar user ne num2 0 dya to ye error show hoga
